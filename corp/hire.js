064ec338fbb40df0541069411f59a70f11190935
Matt Hull <Tigenzero@gmail.com>
2022-05-02 10:30:31 -0400

    updated hire.js

--- corp/hire.js
+++ corp/hire.js
@@ -1,16 +1,17 @@
 /** @param {NS} ns */
 /*
-
+upgrades the office size for a city and adds all new empoyees to the R&D team
 RAM: 1.02TB
 */
 export async function main(ns) {
-	division = args[0]
-	city = args[1]
-	num = args[2]
+	var division = ns.args[0]
+	var city = ns.args[1]
+	var num = ns.args[2]
 	// ns.corporation.expandCity(division)
 	ns.corporation.upgradeOfficeSize(division, city, num)
-	for(i in num){
-		employee = ns.corporation.hireEmployee(division, city)
-		ns.corporation.assignJob(division, city, employee.name, "Research")
+
+	for(let i = 0; i < num; i++){
+		var employee = ns.corporation.hireEmployee(division, city)
+		await ns.corporation.assignJob(division, city, employee.name, "Research & Development")
 	}
 }
