0a8bce05beb7cfbefa7059f046851ea522d6ee10
Matt Hull <Tigenzero@gmail.com>
2024-05-03 10:42:13 -0400

    Delete test directory

--- test/is_script_running.js
+++
@@ -1,8 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-	var script = "/scripts/weaken.script"
-	var server = "home"
-	var target = "n00dles"
-	ns.tprint(ns.isRunning(script, server ,target))
-	// ns.tprint(ns.ps())
-}
